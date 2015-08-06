cfeee06c9f6ff24a9dc9e92110e2cbd74e249a49
michaelpacer <michaelpacer@gmail.com>
2015-08-06 16:47:50 -0700

    fixed print statement

--- pyhawkes/utils/profiling.py
+++ pyhawkes/utils/profiling.py
@@ -15,5 +15,5 @@ try:
     def show_line_stats(stream=None):
         _prof.print_stats(stream=stream)
 except ImportError:
-    print "Failed to load line profiler"
+    print("Failed to load line profiler")
     line_profiled = lambda x: x
